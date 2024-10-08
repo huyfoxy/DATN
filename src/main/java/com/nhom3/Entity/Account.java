6ea3305880f701b9aa1340d4f2faccfd255cbcbe
huyfoxy <huyvqps33814@fpt.edu.vn>
2024-10-09 00:01:54 +0700

    controller,entity

---
+++ src/main/java/com/nhom3/Entity/Account.java
@@ -0,0 +1,16 @@
+package com.nhom3.Entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@AllArgsConstructor
+public class Account {
+   String username  ;
+    String password ;
+
+
+}
